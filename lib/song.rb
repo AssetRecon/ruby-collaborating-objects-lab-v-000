ffc30bd8ac4ca448289012ee4339fc8417af5526
Edsel Caprice <caprice@mail.usf.edu>
2018-07-05 15:07:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
 attr_accessor :name, :artist
 
@@ -7,6 +9,7 @@ end
 
   def artist=(name)
 new_artist = Artist.find_or_create_by_name(name)
+binding.pry
 new_artist.add_song(self)
 @artist = name
 end
